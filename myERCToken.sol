777e759a915e6bbd3b527cdbee12f9f7c851317b
devyanshika <106855016+devyanshika@users.noreply.github.com>
2024-06-30 22:39:13 +0530

    myERCToken.sol

--- myERCToken.sol
+++ myERCToken.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
+
+contract IceCreamToken is ERC20, Ownable {
+    constructor() ERC20("IceCreamToken", "ICT") {
+        _mint(msg.sender, 100 * 10**decimals());
+    }
+
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+
+     
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+  
+}
 
