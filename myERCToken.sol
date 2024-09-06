f4fb407be91fd031d90ace71fbe38e342235417e
devyanshika <106855016+devyanshika@users.noreply.github.com>
2024-09-06 11:24:55 +0530

    Update myERCToken.sol

--- myERCToken.sol
+++ myERCToken.sol
@@ -1,25 +1,87 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
+contract IceCreamToken {
+    string public name = "IceCream";
+    string public symbol = "ICRM";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
 
-contract IceCreamToken is ERC20, Ownable {
-    constructor() ERC20("IceCreamToken", "ICT") {
-        _mint(msg.sender, 100 * 10**decimals());
+    address public owner;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Burn(address indexed burner, uint256 value);
+    event Mint(address indexed to, uint256 value);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only contract owner can call this function");
+        _;
     }
 
-
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+    constructor(uint256 _initialSupply) {
+        owner = msg.sender;
+        mint(owner, _initialSupply); // Mint initial supply to the contract owner
     }
 
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Invalid recipient address");
+        require(_value <= balanceOf[msg.sender], "Insufficient balance");
 
-     
-    function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 
-  
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Invalid recipient address");
+        require(_value <= balanceOf[_from], "Insufficient balance");
+        require(_value <= allowance[_from][msg.sender], "Allowance exceeded");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function mint(address _to, uint256 _value) public onlyOwner {
+        require(_to != address(0), "Invalid address to mint to");
+        totalSupply += _value;
+        balanceOf[_to] += _value;
+
+        emit Mint(_to, _value);
+        emit Transfer(address(0), _to, _value); // Corrected here
+    }
+
+    function burn(uint256 _value) public {
+        require(_value <= balanceOf[msg.sender], "Insufficient balance to burn");
+
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+
+        emit Burn(msg.sender, _value);
+        emit Transfer(msg.sender, address(0), _value);
+    }
+
+    // Explicitly reject Ether transfers
+    receive() external payable {
+        revert("Ether not accepted in this contract");
+    }
+
+    fallback() external payable {
+        revert("Ether not accepted in this contract");
+    }
 }
+
 
